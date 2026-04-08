fa547f188b847280aa618a45b00dbd9a026ffe70
aXs2k00 <stvnsavoy@gmail.com>
2026-04-08 17:16:36 +0000

    Add NTUSER parser export

--- dead_box_harvester/extractors/__init__.py
+++ dead_box_harvester/extractors/__init__.py
@@ -1,5 +1,6 @@
 """Credential extractors"""
 from .base import BaseExtractor
 from .credscan import CredentialFileScanner, scan_for_credentials
+from .ntuser import NTUSERParser, parse_ntuser
 
-__all__ = ["BaseExtractor", "CredentialFileScanner", "scan_for_credentials"]
+__all__ = ["BaseExtractor", "CredentialFileScanner", "scan_for_credentials", "NTUSERParser", "parse_ntuser"]
