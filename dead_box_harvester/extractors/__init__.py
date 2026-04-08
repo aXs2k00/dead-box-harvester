02a8a35c2bc34432c268208d09fee3a3ff0cb610
aXs2k00 <stvnsavoy@gmail.com>
2026-04-08 17:01:39 +0000

    Add file-based credential scanner

--- dead_box_harvester/extractors/__init__.py
+++ dead_box_harvester/extractors/__init__.py
@@ -1,4 +1,5 @@
 """Credential extractors"""
 from .base import BaseExtractor
+from .credscan import CredentialFileScanner, scan_for_credentials
 
-__all__ = ["BaseExtractor"]
+__all__ = ["BaseExtractor", "CredentialFileScanner", "scan_for_credentials"]
